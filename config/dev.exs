316be26b402b1b7bd8bd032431e62ae8465638c7
Gerardo Navarro Suarez <gerardo.navarro.suarez@gmail.com>
2021-01-28 00:30:15 +0100

    Changed port for local testing with mobile device

--- config/dev.exs
+++ config/dev.exs
@@ -17,8 +17,7 @@ config :mindwendel, Mindwendel.Repo,
 # with webpack to recompile .js and .css sources.
 config :mindwendel, MindwendelWeb.Endpoint,
   # Use this when you want to allow external requests
-  # http: [ip: {0, 0, 0, 0}, port: 4000],
-  http: [port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
