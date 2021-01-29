2a9b9ee4da4ba37912ae36ac168d61e0c920a7f7
Jannik Streek <jannik.streek@gmail.com>
2021-01-29 21:26:36 +0100

    fixed bug with stopping run command

--- entrypoint_release.sh
+++ entrypoint_release.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-while ! pg_isready -q -h $DATABASE_HOST -p $DATABASE_PORT -U $DATABASE_USER
-do
-  echo "Waiting for database."
-  sleep 2
-done
-
-exec ./bin/mindwendel eval "Mindwendel.Release.migrate"
-exec ./bin/mindwendel start
+./bin/mindwendel eval "Mindwendel.Release.migrate"
+exec bin/mindwendel start
