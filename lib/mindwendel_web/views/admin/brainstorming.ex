a1bb861cd851ccbc1c4e6a4e0ae17fe2ccac08d1
Jannik Streek <jannik.streek@gmail.com>
2021-01-30 11:32:16 +0100

    :wrench: added export for admin

--- lib/mindwendel_web/views/admin/brainstorming.ex
+++ lib/mindwendel_web/views/admin/brainstorming.ex
@@ -1,3 +1,8 @@
 defmodule MindwendelWeb.Admin.BrainstormingView do
   use MindwendelWeb, :view
+  alias Mindwendel.Brainstormings
+
+  def count_likes_for_idea(idea) do
+    Brainstormings.count_likes_for_idea(idea)
+  end
 end
