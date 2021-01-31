7617a131913bc50c135462a1bf3fac5da0c255b5
Gerardo Navarro Suarez <gerardo.navarro.suarez@gmail.com>
2021-01-31 20:29:30 +0100

    📝 Added docu

--- lib/mindwendel/accounts.ex
+++ lib/mindwendel/accounts.ex
@@ -3,6 +3,19 @@ defmodule Mindwendel.Accounts do
   alias Mindwendel.Accounts.User
   alias Mindwendel.Brainstormings.Brainstorming
 
+  @doc """
+  Finds an existing user or creates a new user based on an UUID.
+
+  Returns nil if User does not exist or any other error is raised.
+
+  Returns nil if invalid UUID is given.
+
+  ## Examples
+
+      iex> get_or_create_user(uuid)
+      %User{}
+
+  """
   def get_or_create_user(id) do
     Repo.get(User, id) ||
       case %User{id: id} |> Repo.insert() do
@@ -10,6 +23,22 @@ defmodule Mindwendel.Accounts do
       end
   end
 
+  @doc """
+  Gets a single user based on its UUID.
+
+  Returns nil if User does not exist or any other error is raised.
+
+  Returns nil if invalid UUID is given.
+
+  ## Examples
+
+      iex> get_user("valid-uuid")
+      %User{}
+
+      iex> get_user("invalid-or-non-existing-uuid")
+      nil
+
+  """
   def get_user(id) when is_nil(id) do
     nil
   end
@@ -20,25 +49,38 @@ defmodule Mindwendel.Accounts do
     Ecto.Query.CastError -> nil
   end
 
+  @doc """
+  Updates a user.
+
+  ## Examples
+
+      iex> update_user(user, %{field: new_value})
+      {:ok, %User{}}
+
+      iex> update_user(user, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
   def update_user(%User{} = user, attrs) do
     user
     |> User.changeset(attrs)
     |> Repo.update()
   end
 
-  def merge_brainstorming_user(%Brainstorming{} = brainstorming, user_id)
-      when is_nil(user_id) do
-    brainstorming
-  end
+  @doc """
+  Connects user to a brainstorm.
 
-  def merge_brainstorming_user(%Brainstorming{} = brainstorming, user_id)
-      when is_binary(user_id) do
-    case Ecto.UUID.dump(user_id) do
-      :error -> brainstorming
-      {:ok, _} -> merge_brainstorming_user(brainstorming, get_or_create_user(user_id))
-    end
-  end
+  Returns a valid brainstorming with preloaded user list.
 
+  ## Examples
+
+      iex> merge_brainstorming_user(brainstorming, user)
+      %Brainstorming{}
+
+      iex> update_user(user, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
   def merge_brainstorming_user(%Brainstorming{} = brainstorming, %User{} = user) do
     unless user.id in Enum.map(brainstorming.users, fn e -> e.id end) do
       brainstorming_users = [user | brainstorming.users]
@@ -54,6 +96,19 @@ defmodule Mindwendel.Accounts do
       end
     else
       brainstorming
+    end
+  end
+
+  def merge_brainstorming_user(%Brainstorming{} = brainstorming, user_id)
+      when is_nil(user_id) do
+    brainstorming
+  end
+
+  def merge_brainstorming_user(%Brainstorming{} = brainstorming, user_id)
+      when is_binary(user_id) do
+    case Ecto.UUID.dump(user_id) do
+      :error -> brainstorming
+      {:ok, _} -> merge_brainstorming_user(brainstorming, get_or_create_user(user_id))
     end
   end
 end
