2b09968baa181d8aad2041fb33940121fb844d41
Gerardo Navarro Suarez <gerardo.navarro.suarez@gmail.com>
2021-02-21 17:03:03 +0100

    build: Allow prod mode

--- config/prod.exs
+++ config/prod.exs
@@ -13,22 +13,27 @@ require Logger
 config :mindwendel, MindwendelWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    Environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+secret_key_base = System.get_env("SECRET_KEY_BASE")
 
-url_host =
-  System.get_env("URL_HOST") ||
-    raise """
-    Environment variable URL_HOST is missing.
-    The URL_HOST should be the domain name (wihtout protocol and port) for accessing your app.
-    """
+unless secret_key_base do
+  Logger.warn(
+    "Environment variable SECRET_KEY_BASE is missing. You can generate one by calling: mix phx.gen.secret"
+  )
+end
+
+url_host = System.get_env("URL_HOST")
+
+unless url_host do
+  Logger.warn(
+    "Environment variable URL_HOST is missing.The URL_HOST should be the domain name (wihtout protocol and port) for accessing your app."
+  )
+end
 
 config :mindwendel, MindwendelWeb.Endpoint,
-  url: [host: url_host, port: 80],
+  url: [
+    host: url_host,
+    port: 80
+  ],
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
@@ -61,33 +66,33 @@ else
     "Environment variable DATABASE_URL is missing. Expecting DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_USER_PASSWORD to be defined"
   )
 
-  database_host =
-    System.get_env("DATABASE_HOST") ||
-      raise """
-      Environment variable DATABASE_HOST is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+  database_host = System.get_env("DATABASE_HOST")
 
-  database_name =
-    System.get_env("DATABASE_NAME") ||
-      raise """
-      Environment variable DATABASE_NAME is missing.
-      For example: mindwendel
-      """
+  unless database_host do
+    Logger.warn(
+      "Environment variable DATABASE_HOST is missing, e.g. ecto://USER:PASS@HOST/DATABASE"
+    )
+  end
 
-  database_user =
-    System.get_env("DATABASE_USER") ||
-      raise """
-      Environment variable DATABASE_USER is missing.
-      For example: mindwendel_user
-      """
+  database_name = System.get_env("DATABASE_NAME")
 
-  database_user_password =
-    System.get_env("DATABASE_USER_PASSWORD") ||
-      raise """
-      Environment variable DATABASE_USER_PASSWORD is missing.
-      For example: password
-      """
+  unless database_name do
+    Logger.warn("Environment variable DATABASE_NAME is missing, e.g. mindwendel_prod_db")
+  end
+
+  database_user = System.get_env("DATABASE_USER")
+
+  unless database_user do
+    Logger.warn("Environment variable DATABASE_USER is missing, e.g. mindwendel_prod_db_user")
+  end
+
+  database_user_password = System.get_env("DATABASE_USER_PASSWORD")
+
+  unless database_user_password do
+    Logger.warn(
+      "Environment variable DATABASE_USER_PASSWORD is missing, e.g. mindwendel_prod_db_user_password"
+    )
+  end
 
   config :mindwendel, Mindwendel.Repo,
     hostname: database_host,
