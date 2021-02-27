71d9db99769a5eb4c296dd61cfb20c7144edc70b
Nicholas Wittstruck <n.wittstruck@gmail.com>
2021-02-27 09:38:47 +0100

    Enables i18n, adds german i18n files

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,10 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+
+# set possible translations:
+config :mindwendel, MindwendelWeb.Gettext, default_locale: "de", locales: ~w(en de)
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
