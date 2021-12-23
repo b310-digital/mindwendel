910b293e8b3929f0458b7e74c5934d97f6ef6f95
Gerardo Navarro <gerar_1989@yahoo.de>
2021-12-23 21:22:58 +0100

    chore: Adjust config for gigalixir withour config/releases.exs

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :mindwendel, Mindwendel.Repo,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
