1c1b8443a5f73f6c7ff2a71908430a52ab62ded7
Gerardo Navarro <gerar_1989@yahoo.de>
2021-12-24 13:17:59 +0100

    chore: Adjust config for gigalixir

--- config/runtime.exs
+++ config/runtime.exs
@@ -8,23 +8,31 @@ require Logger
 if config_env() != :test do
   database_url =
     System.get_env("DATABASE_URL") ||
-      Logger.warn("Environment variable DATABASE_URL is missing, e.g. Database_URL=ecto://USER:PASS@HOST/DATABASE")
+      Logger.warn(
+        "Environment variable DATABASE_URL is missing, e.g. Database_URL=ecto://USER:PASS@HOST/DATABASE"
+      )
 
   database_host =
     System.get_env("DATABASE_HOST") ||
-      Logger.warn("Environment variable DATABASE_HOST is missing, e.g. DATABASE_HOST=localhost or DATABASE_HOST=postgres")
+      Logger.warn(
+        "Environment variable DATABASE_HOST is missing, e.g. DATABASE_HOST=localhost or DATABASE_HOST=postgres"
+      )
 
   database_name =
     System.get_env("DATABASE_NAME") ||
-    Logger.warn("Environment variable DATABASE_NAME is missing, e.g. DATABASE_NAME=mindwendel")
+      Logger.warn("Environment variable DATABASE_NAME is missing, e.g. DATABASE_NAME=mindwendel")
 
-    database_user =
-      System.get_env("DATABASE_USER") ||
-      Logger.warn("Environment variable DATABASE_USER is missing, e.g. DATABASE_USER=mindwendel_user")
+  database_user =
+    System.get_env("DATABASE_USER") ||
+      Logger.warn(
+        "Environment variable DATABASE_USER is missing, e.g. DATABASE_USER=mindwendel_user"
+      )
 
-      database_user_password =
-        System.get_env("DATABASE_USER_PASSWORD") ||
-        Logger.warn("Environment variable DATABASE_USER_PASSWORD is missing, e.g. DATABASE_USER_PASSWORD=mindwendel_user_password")
+  database_user_password =
+    System.get_env("DATABASE_USER_PASSWORD") ||
+      Logger.warn(
+        "Environment variable DATABASE_USER_PASSWORD is missing, e.g. DATABASE_USER_PASSWORD=mindwendel_user_password"
+      )
 
   database_ssl = (System.get_env("DATABASE_SSL") || "true") == "true"
 
@@ -36,7 +44,7 @@ if config_env() != :test do
     port: String.to_integer(System.get_env("DATABASE_PORT") || "5432"),
     ssl: database_ssl,
     url: database_url,
-    username: database_user,
+    username: database_user
 
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
