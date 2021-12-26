4560379dc51e2c7440edc7163f1199eff213181d
Gerardo Navarro <gerar_1989@yahoo.de>
2021-12-26 20:49:23 +0100

    chore: Adjust config for gigalixir

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,45 +6,45 @@ import Config
 require Logger
 
 if config_env() != :test do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      Logger.warn(
-        "Environment variable DATABASE_URL is missing, e.g. Database_URL=ecto://USER:PASS@HOST/DATABASE"
-      )
+  if System.get_env("DATABASE_URL") do
+    Logger.warn(
+      "Environment variable DATABASE_URL is missing, e.g. Database_URL=ecto://USER:PASS@HOST/DATABASE"
+    )
+  end
 
-  database_host =
-    System.get_env("DATABASE_HOST") ||
-      Logger.warn(
-        "Environment variable DATABASE_HOST is missing, e.g. DATABASE_HOST=localhost or DATABASE_HOST=postgres"
-      )
+  if System.get_env("DATABASE_HOST") do
+    Logger.warn(
+      "Environment variable DATABASE_HOST is missing, e.g. DATABASE_HOST=localhost or DATABASE_HOST=postgres"
+    )
+  end
 
-  database_name =
-    System.get_env("DATABASE_NAME") ||
-      Logger.warn("Environment variable DATABASE_NAME is missing, e.g. DATABASE_NAME=mindwendel")
+  if System.get_env("DATABASE_NAME") do
+    Logger.warn("Environment variable DATABASE_NAME is missing, e.g. DATABASE_NAME=mindwendel")
+  end
 
-  database_user =
-    System.get_env("DATABASE_USER") ||
-      Logger.warn(
-        "Environment variable DATABASE_USER is missing, e.g. DATABASE_USER=mindwendel_user"
-      )
+  if System.get_env("DATABASE_USER") do
+    Logger.warn(
+      "Environment variable DATABASE_USER is missing, e.g. DATABASE_USER=mindwendel_user"
+    )
+  end
 
-  database_user_password =
-    System.get_env("DATABASE_USER_PASSWORD") ||
-      Logger.warn(
-        "Environment variable DATABASE_USER_PASSWORD is missing, e.g. DATABASE_USER_PASSWORD=mindwendel_user_password"
-      )
+  if System.get_env("DATABASE_USER_PASSWORD") do
+    Logger.warn(
+      "Environment variable DATABASE_USER_PASSWORD is missing, e.g. DATABASE_USER_PASSWORD=mindwendel_user_password"
+    )
+  end
 
   database_ssl = (System.get_env("DATABASE_SSL") || "true") == "true"
 
   config :mindwendel, Mindwendel.Repo,
-    database: database_name,
-    hostname: database_host,
-    password: database_user_password,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    port: String.to_integer(System.get_env("DATABASE_PORT") || "5432"),
-    ssl: database_ssl,
-    url: database_url,
-    username: database_user
+    database: System.get_env("DATABASE_NAME"),
+    hostname: System.get_env("DATABASE_HOST"),
+    password: System.get_env("DATABASE_USER_PASSWORD"),
+    pool_size: String.to_integer(System.get_env("POOL_SIZE", "10")),
+    port: String.to_integer(System.get_env("DATABASE_PORT", "5432")),
+    ssl: System.get_env("DATABASE_SSL", "true") == "true",
+    url: System.get_env("DATABASE_URL"),
+    username: System.get_env("DATABASE_USER")
 
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
