26532b9fe4749d5ad190f6785792b57120d892ff
JannikStreek <Jannik.streek@gmail.com>
2022-01-03 22:52:51 +0100

    :wrench: added seed job (#41)
    * :wrench: added seed job
    * added generic path creation
    
    Co-authored-by: Gerardo Navarro <gerar_1989@yahoo.de>

--- lib/mindwendel/release.ex
+++ lib/mindwendel/release.ex
@@ -10,6 +10,10 @@ defmodule Mindwendel.Release do
     end
   end
 
+  def run_seeds do
+    Code.eval_file("#{:code.priv_dir(@app)}/repo/seeds.exs")
+  end
+
   def rollback(repo, version) do
     load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
