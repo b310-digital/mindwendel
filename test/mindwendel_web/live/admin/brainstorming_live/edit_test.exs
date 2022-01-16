f8734e57e85eb252eb3828d8d357fb531c84e8af
Gerardo Navarro <gerar_1989@yahoo.de>
2022-01-16 11:15:50 +0100

    chore: Added tests

---
+++ test/mindwendel_web/live/admin/brainstorming_live/edit_test.exs
@@ -0,0 +1,88 @@
+defmodule MindwendelWeb.Admin.BrainstormingLive.EditTest do
+  use MindwendelWeb.ConnCase
+  import Phoenix.LiveViewTest
+
+  alias Mindwendel.Factory
+
+  setup do
+    %{brainstorming: Factory.insert!(:brainstorming)}
+  end
+
+  test "connected mount", %{
+    conn: conn,
+    brainstorming: brainstorming
+  } do
+    {:ok, edit_live_view, _html} =
+      live(conn, Routes.admin_brainstorming_edit_path(conn, :edit, brainstorming.admin_url_id))
+
+    assert render(edit_live_view) =~ brainstorming.name
+  end
+
+  test "submit new brainstorming name", %{
+    conn: conn,
+    brainstorming: brainstorming
+  } do
+    {:ok, edit_live_view, _html} =
+      live(conn, Routes.admin_brainstorming_edit_path(conn, :edit, brainstorming.admin_url_id))
+
+    assert edit_live_view
+           |> element("form")
+           |> render_submit(%{
+             brainstorming: %{name: "New brainstorming name"}
+           }) =~
+             "New brainstorming name"
+  end
+
+  test "renders five idea labels for brainstorming", %{
+    conn: conn,
+    brainstorming: brainstorming
+  } do
+    {:ok, edit_live_view, _html} =
+      live(conn, Routes.admin_brainstorming_edit_path(conn, :edit, brainstorming.admin_url_id))
+
+    assert edit_live_view |> element("input#brainstorming_labels_0_name") |> has_element?
+    assert edit_live_view |> element("input#brainstorming_labels_1_name") |> has_element?
+    assert edit_live_view |> element("input#brainstorming_labels_2_name") |> has_element?
+    assert edit_live_view |> element("input#brainstorming_labels_3_name") |> has_element?
+    assert edit_live_view |> element("input#brainstorming_labels_4_name") |> has_element?
+    refute edit_live_view |> element("input#brainstorming_labels_5_name") |> has_element?
+  end
+
+  test "adds and immediately saves new idea label", %{
+    conn: conn,
+    brainstorming: brainstorming
+  } do
+    {:ok, edit_live_view, _html} =
+      live(conn, Routes.admin_brainstorming_edit_path(conn, :edit, brainstorming.admin_url_id))
+
+    edit_live_view
+    |> element("button", "Add idea label")
+    |> render_click()
+
+    assert edit_live_view |> element("input#brainstorming_labels_5_name") |> has_element?
+    refute edit_live_view |> element("input#brainstorming_labels_6_name") |> has_element?
+
+    edit_live_view
+    |> element("button", "Add idea label")
+    |> render_click()
+
+    assert edit_live_view |> element("input#brainstorming_labels_6_name") |> has_element?
+  end
+
+  test "removes idea label", %{
+    conn: conn,
+    brainstorming: brainstorming
+  } do
+    {:ok, edit_live_view, _html} =
+      live(conn, Routes.admin_brainstorming_edit_path(conn, :edit, brainstorming.admin_url_id))
+
+    IO.inspect(Enum.at(brainstorming.labels, 0).id)
+
+    edit_live_view
+    |> element("button[value=#{Enum.at(brainstorming.labels, 0).id}]", "Remove")
+    |> render_click()
+
+    assert edit_live_view |> element("input#brainstorming_labels_0_name") |> has_element?
+    refute edit_live_view |> element("input#brainstorming_labels_4_name") |> has_element?
+  end
+end
