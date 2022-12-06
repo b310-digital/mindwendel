0887cf3544540b0706e392a99e31c8486a97d115
Gerardo Navarro <gerardo@b310.de>
2022-12-06 11:00:19 +0000

    refactor: Fix kits landing page

--- lib/mindwendel_web/endpoint.ex
+++ lib/mindwendel_web/endpoint.ex
@@ -24,7 +24,24 @@ defmodule MindwendelWeb.Endpoint do
     at: "/",
     from: :mindwendel,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: [
+      "assets",
+      "apple-touch-icon.png",
+      "css",
+      "favicon.ico",
+      "fonts",
+      "images",
+      "js",
+      "robots.txt",
+      "browserconfig.xml",
+      "safari-pinned-tab.svg",
+      "site.webmanifest"
+    ],
+    only_matching: [
+      "android-chrome-",
+      "favicon-",
+      "mstile-"
+    ]
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
