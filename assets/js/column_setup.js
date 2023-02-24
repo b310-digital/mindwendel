96688de41e161dcf3af8a547464340cfdc02e584
Nicholas <nwittstruck@users.noreply.github.com>
2023-02-24 18:03:38 +0100

    [Bug] Fixes issue with the dropdown in the menu bar that shows old brainstormings (#26)

--- assets/js/column_setup.js
+++ assets/js/column_setup.js
@@ -1,17 +1,3 @@
-import "bootstrap/js/dist/alert";
-import "bootstrap/js/dist/button";
-import "bootstrap/js/dist/collapse";
-import "bootstrap/js/dist/dropdown";
-import Tooltip from "bootstrap/js/dist/tooltip";
-
-// activate all tooltips:
-const tooltipTriggerList = document.querySelectorAll(
-  '[data-bs-toggle="tooltip"]'
-);
-const tooltipList = [...tooltipTriggerList].map(
-  (tooltipTriggerEl) => new Tooltip(tooltipTriggerEl)
-);
-
 // configure link list - on iOS, the navigation bar is dynamic. when it's expanded,
 // the link list would be below the fold or very close to the edge.
 // to handle this, we adjust the size of the column to the inner height of the window
