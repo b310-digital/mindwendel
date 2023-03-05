03a7b285980bf71eb728624dc287df5ed9890ad7
Gerardo Navarro <gerardo@b310.de>
2023-03-05 14:55:19 +0100

    style: Increase readability

--- config/config.exs
+++ config/config.exs
@@ -39,8 +39,15 @@ config :phoenix, :json_library, Jason
 config :esbuild,
   version: "0.19.11",
   default: [
-    args:
-      ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets --external:/images/* --loader:.woff=file --loader:.woff2=file),
+    args: ~w(
+      js/app.js
+      --bundle
+      --target=es2016
+      --outdir=../priv/static/assets
+      --external:/images/*
+      --loader:.woff=file
+      --loader:.woff2=file
+    ),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
