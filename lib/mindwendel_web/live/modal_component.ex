9705fd44287289e4a58c09b8a8a84a08849f0974
nwittstruck <nwittstruck@users.noreply.github.com>
2024-01-25 13:15:34 +0100

    chore: upgrade phoenix to 1.7.10

--- lib/mindwendel_web/live/modal_component.ex
+++ lib/mindwendel_web/live/modal_component.ex
@@ -4,22 +4,26 @@ defmodule MindwendelWeb.ModalComponent do
   @impl true
   def render(assigns) do
     ~H"""
-    <div id={@id} class="modal fade show" tabindex="-1" phx-hook="Modal"
-        phx-target={"##{ @myself }"}
-        phx-page-loading
-      >
-        <div class={"modal-dialog #{assigns.opts[:modal_size]}"} role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title"><%= assigns.opts[:title] %></h5>
-              <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
-            </div>
-            <div class="modal-body">
-              <%= live_component @component, @opts %>
-            </div>
+    <div
+      id={@id}
+      class="modal fade show"
+      tabindex="-1"
+      phx-hook="Modal"
+      phx-target={"##{ @myself }"}
+      phx-page-loading
+    >
+      <div class={"modal-dialog #{assigns.opts[:modal_size]}"} role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title"><%= assigns.opts[:title] %></h5>
+            <%= live_patch(raw("&times;"), to: @return_to, class: "phx-modal-close") %>
+          </div>
+          <div class="modal-body">
+            <%= live_component(@component, @opts) %>
           </div>
         </div>
       </div>
+    </div>
     """
   end
 
