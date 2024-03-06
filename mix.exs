9ad294e3a1ea2b44ffbd841925a58dd4ad12110f
Nicholas <nwittstruck@users.noreply.github.com>
2024-03-06 13:02:30 +0100

    chore: bump version to 0.1.2 in preparation for next release (#253)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mindwendel.MixProject do
   def project do
     [
       app: :mindwendel,
-      version: "0.1.0",
+      version: "0.1.2",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
