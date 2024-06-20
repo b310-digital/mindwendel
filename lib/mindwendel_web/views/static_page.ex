6f934644f3a88826a51d38e5a998bf8bc05e074b
nwittstruck <nwittstruck@users.noreply.github.com>
2024-06-20 12:44:22 +0200

    feature: show deletion date warning on home page #319

--- lib/mindwendel_web/views/static_page.ex
+++ lib/mindwendel_web/views/static_page.ex
@@ -5,4 +5,11 @@ defmodule MindwendelWeb.StaticPageView do
   def list_brainstormings_for(user) do
     Brainstormings.list_brainstormings_for(user.id)
   end
+
+  def brainstormings_available_until() do
+    Timex.Duration.from_days(
+      Application.fetch_env!(:mindwendel, :options)[:feature_brainstorming_removal_after_days]
+    )
+    |> Timex.format_duration(:humanized)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mindwendel.MixProject do
   def project do
     [
       app: :mindwendel,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
