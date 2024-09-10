3c8bc4602da3e66b440b9334f40813a0286b8d29
JannikStreek <Jannik.streek@gmail.com>
2024-09-10 15:53:57 +0200

    minor UI adjustments  (#335)

--- lib/mindwendel_web/views/layout_view.ex
+++ lib/mindwendel_web/views/layout_view.ex
@@ -5,4 +5,9 @@ defmodule MindwendelWeb.LayoutView do
   def list_brainstormings_for(user, limit \\ 3) do
     Brainstormings.list_brainstormings_for(user.id, limit)
   end
+
+  def admin_route(conn) do
+    route_scope = conn.request_path |> String.split("/", trim: true) |> List.first()
+    route_scope == "admin"
+  end
 end
