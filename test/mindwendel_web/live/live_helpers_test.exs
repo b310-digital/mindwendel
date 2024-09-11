21cea6a593c3fe43aa35dcf6b2f14557e3f77845
Jannik Streek <jannik.streek@gmail.com>
2024-09-11 10:41:24 +0200

    fix tests

--- test/mindwendel_web/live/live_helpers_test.exs
+++ test/mindwendel_web/live/live_helpers_test.exs
@@ -1,6 +1,7 @@
 defmodule MindwendelWeb.LiveHelpersTest do
   use MindwendelWeb.ConnCase
   import Phoenix.LiveViewTest
+  alias Mindwendel.Brainstormings
 
   alias Mindwendel.Factory
 
@@ -16,5 +17,20 @@ defmodule MindwendelWeb.LiveHelpersTest do
       live(conn, Routes.brainstorming_show_path(conn, :show, brainstorming))
 
     assert html =~ "in 29 days"
+  end
+
+  test "contains sort button by likes for admin", %{
+    conn: conn,
+    brainstorming: brainstorming
+  } do
+    moderating_user = Factory.insert!(:user)
+    Brainstormings.add_moderating_user(brainstorming, moderating_user)
+
+    {:ok, view, _html} =
+      conn
+      |> init_test_session(%{current_user_id: moderating_user.id})
+      |> live(Routes.brainstorming_show_path(conn, :show, brainstorming))
+
+    assert view |> has_element?(".btn[title|='Sort by likes']")
   end
 end
