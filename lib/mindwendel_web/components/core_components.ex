859a6e3628685f26e0d1e64e597056273a395bd6
Jannik Streek <jannik.streek@gmail.com>
2024-09-30 11:05:45 +0200

    improve lane ux

--- lib/mindwendel_web/components/core_components.ex
+++ lib/mindwendel_web/components/core_components.ex
@@ -720,7 +720,8 @@ defmodule MindwendelWeb.CoreComponents do
     <div class={[
       @lane_count == 1 && "lane lane--full-width",
       @lane_count == 2 && "lane lane--half-width",
-      @lane_count >= 3 && "lane lane--column-width",
+      @lane_count == 3 && "lane lane--third-width",
+      @lane_count > 3 && "lane lane--column-width",
       @class
     ]}>
       <%= render_slot(@inner_block) %>
