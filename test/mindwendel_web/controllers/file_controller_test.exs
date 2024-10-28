e7ae18a30cdbda6f133449e4f76dda5485e89313
Jannik Streek <jannik.streek@gmail.com>
2024-10-28 14:25:08 +0100

    refactor

--- test/mindwendel_web/controllers/file_controller_test.exs
+++ test/mindwendel_web/controllers/file_controller_test.exs
@@ -3,13 +3,10 @@ defmodule MindwendelWeb.FileControllerTest do
   alias Mindwendel.Services.Vault
   alias Mindwendel.Factory
 
-  @file_dest "priv/static/uploads/encrypted-test.jpg"
+  @file_dest "priv/static/uploads/encrypted-file-controller-test.jpg"
 
   describe "get_file" do
     setup do
-      upload_path = "priv/static/uploads/"
-      File.mkdir_p!(Path.dirname(upload_path))
-
       {:ok, encrypted_file} = Vault.encrypt("test")
       File.write(@file_dest, encrypted_file)
 
@@ -18,7 +15,7 @@ defmodule MindwendelWeb.FileControllerTest do
     end
 
     test "successfully retrieves an existing file", %{conn: conn} do
-      file = Factory.insert!(:file, path: "/uploads/encrypted-test.jpg", name: "test.jpg")
+      file = Factory.insert!(:file, path: "/uploads/encrypted-file-controller-test.jpg", name: "test.jpg")
 
       assert get(conn, ~p"/files/#{file.id}").resp_body == "test"
     end
