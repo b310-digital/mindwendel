7a7482cb96f98722d6d73ce7bca2d0014e0edd28
Jannik Streek <jannik.streek@gmail.com>
2024-11-07 20:54:24 +0100

    delete comment

--- lib/mindwendel_web/live/comment_live/show_component.ex
+++ lib/mindwendel_web/live/comment_live/show_component.ex
@@ -1,8 +1,16 @@
 defmodule MindwendelWeb.CommentLive.ShowComponent do
   use MindwendelWeb, :live_component
+  alias Mindwendel.Comments
 
   @impl true
   def handle_event("edit_comment", _value, socket) do
     {:noreply, assign(socket, :live_action, :edit)}
+  end
+
+  @impl true
+  def handle_event("delete_comment", _, socket) do
+    Comments.delete_comment(socket.assigns.comment)
+
+    {:noreply, socket}
   end
 end
