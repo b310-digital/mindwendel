bcb8f2e3740dc3b60055ce443ee092f690b621be
Jannik Streek <jannik.streek@gmail.com>
2024-11-09 13:10:33 +0100

    adjust configuration for scalingo

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,7 +6,6 @@ import Config
 require Logger
 
 if config_env() == :prod do
-  # configure logging:
   config :logger, :default_handler,
     formatter: {
       LoggerJSON.Formatters.Basic,
@@ -49,13 +48,28 @@ if config_env() != :test do
   # disable on prod, because logger_json will take care of this. set to :debug for test and dev
   ecto_log_level = if config_env() == :prod, do: false, else: :debug
 
-  ssl_config =
-    if System.get_env("DATABASE_SSL", "true") == "true",
-      do: [cacerts: :public_key.cacerts_get()],
-      else: nil
+  # default ssl_opts:
+  ssl_opts = [
+    verify: :verify_peer,
+    depth: 3,
+    versions: [:"tlsv1.3"],
+    server_name_indication: String.to_charlist(System.get_env("DATABASE_HOST")),
+    customize_hostname_check: [
+      match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+    ]
+  ]
+
+  # either use system certificates or specify files:
+  ssl_opts =
+    if System.get_env("DATABASE_CERT_FILE") do
+      Logger.info("Loading DATABASE_CERT_FILE")
+      ssl_opts ++ [cacertfile: System.get_env("DATABASE_CERT_FILE")]
+    else
+      Logger.info("Loading System Certificates")
+      ssl_opts ++ [cacerts: :public_key.cacerts_get()]
+    end
 
   config :mindwendel, Mindwendel.Repo,
-    start_apps_before_migration: [:logger_json],
     database: System.get_env("DATABASE_NAME"),
     hostname: System.get_env("DATABASE_HOST"),
     password: System.get_env("DATABASE_USER_PASSWORD"),
@@ -65,7 +79,8 @@ if config_env() != :test do
     url: System.get_env("DATABASE_URL"),
     timeout: String.to_integer(System.get_env("DATABASE_TIMEOUT", "15000")),
     log: ecto_log_level,
-    ssl: ssl_config
+    ssl: System.get_env("DATABASE_SSL", "true") == "true",
+    ssl_opts: ssl_opts
 
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
