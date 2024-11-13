22fbf50f53ff2bcbb4da543d1288c24f6dfa9b08
Tobias Pfeiffer <pragtob@gmail.com>
2024-11-13 10:21:17 +0100

    use new migration file to keep logical order of migrations

--- priv/repo/migrations/20241005062228_remove_unused_fields.exs
+++
@@ -1,9 +0,0 @@
-defmodule Mindwendel.Repo.Migrations.RemoveUnusedFields do
-  use Ecto.Migration
-
-  def change do
-    alter table(:ideas) do
-      remove :label, :text
-    end
-  end
-end
--- priv/repo/migrations/20241113091936_remove_unused_label_fields.exs
+++ priv/repo/migrations/20241113091936_remove_unused_label_fields.exs
@@ -1,8 +1,9 @@
-defmodule Mindwendel.Repo.Migrations.RemoveLabelIdFromIdeas do
+defmodule Mindwendel.Repo.Migrations.RemoveUnusedLabelFields do
   use Ecto.Migration
 
   def change do
     alter table(:ideas) do
+      remove :label, :text
       remove :label_id, references(:idea_labels, type: :uuid)
     end
   end
