b630d84ea9ebec3e5a72157a5a85a1929a4eda84
Tobias Pfeiffer <pragtob@gmail.com>
2024-11-30 18:22:21 +0100

    Move where the feature flag is checked for random inspiration
    This one might feel odd. Well, it just felt odd to me that the
    function would be called and then just return `nil` if the
    feature flag was deactivated. I felt the function shouldn't
    be called at all.
    
    This way, for instance, if it was deactivated in the test
    environment we could still test it etc - the behavior is still
    there.
    
    That's all debatable and more of a "I feel like this" kind of
    thing.

--- lib/mindwendel/help.ex
+++ lib/mindwendel/help.ex
@@ -4,13 +4,10 @@ defmodule Mindwendel.Help do
   """
 
   import Ecto.Query, warn: false
-  alias Mindwendel.FeatureFlag
   alias Mindwendel.Repo
   alias Mindwendel.Help.Inspiration
 
   def random_inspiration do
-    if FeatureFlag.enabled?(:feature_brainstorming_teasers) do
-      Repo.one(from t in Inspiration, order_by: fragment("RANDOM()"), limit: 1)
-    end
+    Repo.one(from t in Inspiration, order_by: fragment("RANDOM()"), limit: 1)
   end
 end
